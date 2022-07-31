34feb3c77efc34e897f3161b89d05ab7fbdb63f5
BryanOrtega1 <110350980+BryanOrtega1@users.noreply.github.com>
2022-07-31 12:35:31 -0600

    Add files via upload

---
+++ TareaLaboratorio-1.py
@@ -0,0 +1,74 @@
+class casa:
+    def cuarto(self):
+        print("En el cuarto encontramos: ")
+        print("Estante")
+        print("Cama")
+        print("Television")
+        print("Puerta")
+
+    def baño(self):
+        print("En el baño encontramos: ")
+        print("Ducha")
+        print("Inodoro")
+        print("Lavamanos")
+        print("Puerta")
+
+    def cocina(self):
+        print("En la cocina encontramos: ")
+        print("Refrigerador")
+        print("Estufa")
+        print("Lavaplatos")
+        print("Alacena")
+
+    def comedor(self):
+        print("En el comedor encotramos: ")
+        print("Mesa")
+        print("Sillas")
+        print("Trinchante")
+
+    def sala(self):
+        print("En la sala encontramos: ")
+        print("Sillon")
+        print("Mueble de television")
+        print("Television")
+        print("Mesa de sala")
+
+    def garaje(self):
+        print("En el garaje encontramos: ")
+        print("Puerta")
+        print("Carros")
+        print("Almacen de herramientas")
+
+    def jardin(self):
+        print("En el jardin encontramos:")
+        print("Flores")
+        print("Grama")
+        print("Masetas")
+
+    def estructura(self):
+        print("En la estructura de la casa se encuentra")
+        print("cimientos")
+        print("columnas")
+        print("vigas")
+        print("paredes interiores")
+        print("paredes exteriores")
+
+
+obj1 = casa()
+print("La casa")
+print(" ")
+casa.garaje(0)
+print(" ")
+casa.cocina(0)
+print(" ")
+casa.comedor(0)
+print(" ")
+casa.sala(0)
+print(" ")
+casa.cuarto(0)
+print(" ")
+casa.baño(0)
+print(" ")
+casa.jardin(0)
+print(" ")
+casa.estructura(0)
